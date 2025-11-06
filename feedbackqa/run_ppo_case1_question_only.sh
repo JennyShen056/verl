bcb978fa4d62615ed3ff27e3e8c2979166a04daf
JennyShen056 <jennyshen056@gmail.com>
2025-11-06 13:53:25 -0800

    update

--- feedbackqa/run_ppo_case1_question_only.sh
+++ feedbackqa/run_ppo_case1_question_only.sh
@@ -57,6 +57,7 @@ python3 -m verl.trainer.main_ppo \
     critic.model.fsdp_config.optimizer_offload=False \
     reward_model.enable=True \
     reward_model.model.path="$HOME/models/FsfairX-LLaMA3-RM-v0.1" \
+    reward_model.model.input_tokenizer=null \
     reward_model.model.use_remove_padding=True \
     reward_model.model.fsdp_config.param_offload=True \
     reward_model.micro_batch_size_per_gpu=16 \
