09296611d37949d50ba7b591265c47a173d7774d
JennyShen056 <jennyshen056@gmail.com>
2025-11-12 18:49:04 -0800

    update

--- helpsteer3/run_ppo_case1_question_only.sh
+++ helpsteer3/run_ppo_case1_question_only.sh
@@ -57,7 +57,7 @@ python3 -m verl.trainer.main_ppo \
     critic.model.fsdp_config.optimizer_offload=False \
     reward_model.enable=True \
     reward_model.model.path="$HOME/models/llama3_help_rm" \
-    reward_model.model.input_tokenizer=null \
+    reward_model.model.input_tokenizer="meta-llama/Llama-3.1-8B-Instruct" \
     reward_model.model.use_remove_padding=True \
     reward_model.model.fsdp_config.param_offload=True \
     reward_model.micro_batch_size_per_gpu=16 \
